43c92c735c9ca70f572ce984094b218a988571a3
andreiivanov891 <99356079+andreiivanov891@users.noreply.github.com>
2022-02-09 21:28:06 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int busnumber;
+    cin >> busnumber;
+    busnumber = busnumber + 100;
+    cout << busnumber;
+}
